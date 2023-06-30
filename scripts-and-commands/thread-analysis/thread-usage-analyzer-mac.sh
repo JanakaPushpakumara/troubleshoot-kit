7a16da61875b3769e9a136109f4f026169305b28
janithcmw <janithcmw@gmail.com>
2023-06-30 18:23:42 +0530

    add thread usage script for mac-os.

---
+++ scripts-and-commands/thread-analysis/thread-usage-analyzer-mac.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]; then
+        echo "usage: sh ThreadDumpUsageAnalyzer.sh <PID> <high CPU thread result count>"
+        exit
+fi
+
+PID=$1
+RESULTCOUNT=$2
+
+echo  "*****  START ***** ";
+for f in thread_usage*.txt;
+do
+        # echo 'start of thead usesage analysis' $f;
+        DATETIME=$( echo ${f} | cut -b 14- )
+        #!echo "$DATETIME"
+        TDDumpL=$(find . -name "thread_dump_$DATETIME");
+        TDDumpS=$(echo $TDDumpL | cut -d '/' -f 2)
+        echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
+        echo 'start of the analysis for the file' $TDDumpS '-' $f;
+        #! View thread usage column names
+        echo "  PID   TID %CPU     TIME NLWP  C | JAVAThread ";
+        #!echo " " ${f};
+        VAR=$(grep $PID $f | sort -nk3 | tail -n $RESULTCOUNT )
+        grep $PID $f | sort -nk3 | tail -n $RESULTCOUNT | while read usage_line ;
+        do
+        	printf "$usage_line";
+        	#! Native OS and JVA thread ID mapping
+        	NTID=$(echo "$usage_line" | awk  '{print $2}' );
+        	NTIDHEX=$(echo "obase=16; $NTID "| bc | awk '{print tolower($0)}');
+
+        	#! View decimal to hexadecimal mapping
+        	#!echo -n " | $NTID";
+        	#!echo -n " Hexa>> ";
+        	#!echo -n "$NTIDHEX | ";
+
+        	printf " | ";
+        	grep "nid=0x$NTIDHEX" $TDDumpL;
+
+        done;
+        echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
+done;
+echo  "*****  END ***** ";
+echo "";
